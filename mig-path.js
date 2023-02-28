f6debe84a58dcbd608bac57b46e6198ff46b1d11
doa.sbeen <doa.sbeen@gmail.com>
2023-02-28 13:10:16 +0900

    window file path add

--- mig-path.js
+++ mig-path.js
@@ -4,13 +4,17 @@ const migPath = {};
 
 /* 경로는 window, mac의 폴더 구분자가 틀림 */
 
+
 // PDF 떨굴 앞의 경로
-migPath.PREFIX_PATH_PDF_BODY = "/Users/yongsungahn/pdf";
+// /Users/yongsungahn/pdf, D:\\mig-data\\pdf
+migPath.PREFIX_PATH_PDF_BODY = "D:\\mig-data\\pdf";
 
 // 기존 첨부파일의 경로 : prefix
-migPath.PREFIX_PATH_OLD_ATTACH = "/Users/yongsungahn/attach/old";
+// /Users/yongsungahn/attach/old, D:\\mig-data\\attach\\old
+migPath.PREFIX_PATH_OLD_ATTACH = "D:\\mig-data\\attach\\old";
 
 // copy할 첨부파일의 경로 : prefix
-migPath.PREFIX_PATH_NEW_ATTACH = "/Users/yongsungahn/attach/new";
+// /Users/yongsungahn/attach/new, D:\\mig-data\\attach\\new
+migPath.PREFIX_PATH_NEW_ATTACH = "D:\\mig-data\\attach\\new";
 
 module.exports = migPath;
