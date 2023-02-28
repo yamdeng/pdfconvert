779dcdbdd9ab752ae3f49c6ebfbaee628b3129cf
doa.sbeen <doa.sbeen@gmail.com>
2023-02-28 13:25:39 +0900

    mig path real

--- mig-path.js
+++ mig-path.js
@@ -7,14 +7,17 @@ const migPath = {};
 
 // PDF 떨굴 앞의 경로
 // /Users/yongsungahn/pdf, D:\\mig-data\\pdf
-migPath.PREFIX_PATH_PDF_BODY = "D:\\mig-data\\pdf";
+// office5 os server : G:\\mig-data\\pdf
+migPath.PREFIX_PATH_PDF_BODY = "G:\\mig-data\\pdf";
 
 // 기존 첨부파일의 경로 : prefix
 // /Users/yongsungahn/attach/old, D:\\mig-data\\attach\\old
-migPath.PREFIX_PATH_OLD_ATTACH = "D:\\mig-data\\attach\\old";
+// office5 os server : D:\\SpeedOffice\\data\\C_DAERYUK\\app\\temp
+migPath.PREFIX_PATH_OLD_ATTACH = "G:\\mig-data\\old";
 
 // copy할 첨부파일의 경로 : prefix
 // /Users/yongsungahn/attach/new, D:\\mig-data\\attach\\new
-migPath.PREFIX_PATH_NEW_ATTACH = "D:\\mig-data\\attach\\new";
+// office5 os server : G:\\mig-data\\attach
+migPath.PREFIX_PATH_NEW_ATTACH = "G:\\mig-data\\attach";
 
 module.exports = migPath;
