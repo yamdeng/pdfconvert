80b127c7b82100d1b403bfdb2f929ac88867c5e7
doa.sbeen <doa.sbeen@gmail.com>
2023-03-02 11:35:34 +0900

    office5, temp 정리

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ office6 운영서버 61.40.65.14 : administrator / Tkddn2363
 office6 oracle 설치 서버 61.40.65.15 : administrator / Tkddn2363
 office6 oracle db 61.40.65.15:1521:orcl : gwdb / tkddn2363
 office6 web http://61.40.65.14/office6 admingw / Speed66^^
+
+
+// var firstConvertDeptName = deptname.replace('dept.', '');
+// var finalDeptName = firstConvertDeptName.replace('/C_DAERYUK', '');
+
+// dept.관리팀/C_DAERYUK
+// dept.영업1팀/C_DAERYUK
+// dept.영업3팀/C_DAERYUK
--- sql/office5.sql
+++ sql/office5.sql
@@ -1,26 +1,42 @@
--- 연관 테이블
--- OA_EAPP_DEPTDOCU : 부서벼별 매핑 정보
--- O0_USER_MAST : 사용자 테이블
--- O0_USER_DEPT : 부서 테이블
--- OA_EAPP_DOCUMAS : 결재문서 마스터
--- userid, docuno, deptcode, formcode, regdate, title, status
--- status : P(progress), C(complete), A(cancel), B(banryeo), T(temp)
--- OA_EAPP_FORM : 기안템플릿 정보
--- formcode, formname
--- OA_EAPP_APP : 결재정보
--- docuno, userid, APPSPEC, APPSTATUS
-SELECT
-    docuno,
-    userid,
-    APPSPEC,
-    APPSTATUS
-FROM
-    OA_EAPP_APP
-WHERE
-    DOCUNO = '202302241454380957';
+-- 부서 테이블
+SELECT *
+FROM O0_USER_DEPT
+WHERE DEPTCODE =  '37';
 
-SELECT
-    formcode,
-    formname
-FROM
-    OA_EAPP_FORM;
+-- 사용자 테이블
+SELECT *
+FROM O0_USER_MAST
+WHERE userid = 'ijeong22';
+
+-- 부서함 매핑 테이블
+SELECT *
+FROM OA_EAPP_DEPTDOCU;
+WHERE BOXCODE ='BX';
+
+-- 문서 마스터 테이블
+SELECT *
+FROM OA_EAPP_DOCUMAS oed
+WHERE DOCUNO = '201907080855190133';
+
+-- 페이징 쿼리
+SELECT *
+FROM (
+	SELECT *
+	FROM OA_EAPP_DOCUMAS
+	ORDER BY docuno ASC
+) A
+WHERE ROWNUM <= 100;
+
+-- 양식 템플릿 테이블
+SELECT *
+FROM OA_EAPP_FORM;
+
+-- 문서번호로 비교해서 추출하기
+SELECT *
+	FROM OA_EAPP_DOCUMAS
+	WHERE docuno > '201409020938230083' AND DOCUNO < '201409020939190554';
+
+-- 첨부파일 조회
+SELECT *
+FROM OA_EAPP_ATTACHFILE
+WHERE FILECODE  = 'ERPCODE';
