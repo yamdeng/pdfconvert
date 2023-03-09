f5952a7cc22cf395b6361f9b8ab2960d562812a2
yamdeng <yamdeng@gmail.com>
2023-03-09 15:08:48 +0900

    file-list :only-log apply

--- test/basic/file-list.js
+++ test/basic/file-list.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const fs = require("fs");
+const logger = require("../../logger-only");
 const directoryPath = "/Users/yongsungahn/Project/pdfconvert";
 
 fs.readdir(directoryPath, function (err, files) {
@@ -8,7 +9,6 @@ fs.readdir(directoryPath, function (err, files) {
     return console.log("Unable to scan directory: " + err);
   }
   files.forEach(function (file) {
-    console.log(file);
-    // console.log("filename : " + file);
+    logger.info(file);
   });
 });
